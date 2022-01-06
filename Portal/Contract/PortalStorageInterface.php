b3db19bebd67485e3e1e4c866ffe0f9352fc478d
Joshua Behrens <behrens@heptacom.de>
2022-01-06 03:29:53 +0100

    Add HttpClientUtilityFactoryInterface to create HTTP client tooling for portals
    CONNECT-200

---
+++ Portal/Contract/PortalStorageInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Portal\Contract;
+
+use Psr\SimpleCache\CacheInterface;
+
+interface PortalStorageInterface extends CacheInterface
+{
+    public function list(): iterable;
+
+    public function canGet(string $type): bool;
+
+    public function canSet(string $type): bool;
+}
