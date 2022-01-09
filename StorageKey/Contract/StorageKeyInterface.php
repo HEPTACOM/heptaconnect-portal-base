81da2b9a549f136819e6b72defef1bf8478dfb81
Julian Krzefski <krzefski@heptacom.de>
2022-01-09 20:31:23 +0100

    CONNECT-200: Re-define HttpClientContract and add default implementation

---
+++ StorageKey/Contract/StorageKeyInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract;
+
+interface StorageKeyInterface extends \JsonSerializable
+{
+    public function equals(StorageKeyInterface $other): bool;
+}
