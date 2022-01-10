0a612d60e1de07ca58333a3f302c887f9fb224d2
Joshua Behrens <behrens@heptacom.de>
2022-01-10 04:41:51 +0100

    Add route capability action to storage facade
    CONNECT-126

---
+++ Builder/Token/StatusReporterToken.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Builder\Token;
+
+class StatusReporterToken
+{
+    private string $topic;
+
+    private ?\Closure $run = null;
+
+    public function __construct(string $topic)
+    {
+        $this->topic = $topic;
+    }
+
+    public function getTopic(): string
+    {
+        return $this->topic;
+    }
+
+    public function getRun(): ?\Closure
+    {
+        return $this->run;
+    }
+
+    public function setRun(\Closure $run): void
+    {
+        $this->run = $run;
+    }
+}
---
+++ Cronjob/Contract/CronjobHandlerContract.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Cronjob\Contract;
+
+/**
+ * @internal
+ */
+abstract class CronjobHandlerContract
+{
+    abstract public function handle(CronjobContextInterface $context): void;
+
+    abstract protected function getDecorated(): CronjobHandlerContract;
+}
