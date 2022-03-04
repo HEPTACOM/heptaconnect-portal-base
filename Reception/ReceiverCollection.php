a5bb482e110eed8fffc6661ca3afcff15ccb0850
Joshua Behrens <behrens@heptacom.de>
2022-03-04 09:28:12 +0100

    Replace writing PortalNodeStorage method with storage action
    CONNECT-126

---
+++ Reception/ReceiverCollection.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Reception;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
+use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractObjectCollection;
+use Heptacom\HeptaConnect\Portal\Base\Reception\Contract\ReceiverContract;
+
+/**
+ * @extends AbstractObjectCollection<ReceiverContract>
+ */
+class ReceiverCollection extends AbstractObjectCollection
+{
+    /**
+     * @param class-string<DatasetEntityContract> $entityType
+     *
+     * @return iterable<int, ReceiverContract>
+     */
+    public function bySupport(string $entityType): iterable
+    {
+        return $this->filter(fn (ReceiverContract $emitter) => $entityType === $emitter->supports());
+    }
+
+    /**
+     * @psalm-return Contract\ReceiverContract::class
+     */
+    protected function getT(): string
+    {
+        return ReceiverContract::class;
+    }
+}
