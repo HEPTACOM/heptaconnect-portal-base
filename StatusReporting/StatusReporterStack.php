0440376275cf0dcdcd909c2a0fe86ae306a7c991
Julian Krzefski <krzefski@heptacom.de>
2022-10-14 20:11:00 +0200

    Add todo comment

---
+++ StatusReporting/StatusReporterStack.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\StatusReporting;
+
+use Heptacom\HeptaConnect\Portal\Base\StatusReporting\Contract\StatusReporterContract;
+use Heptacom\HeptaConnect\Portal\Base\StatusReporting\Contract\StatusReporterStackInterface;
+use Heptacom\HeptaConnect\Portal\Base\StatusReporting\Contract\StatusReportingContextInterface;
+use Psr\Log\LoggerInterface;
+
+final class StatusReporterStack implements StatusReporterStackInterface
+{
+    /**
+     * @var array<array-key, StatusReporterContract>
+     */
+    private array $statusReporters;
+
+    private LoggerInterface $logger;
+
+    /**
+     * @param iterable<array-key, StatusReporterContract> $statusReporters
+     */
+    public function __construct(iterable $statusReporters, LoggerInterface $logger)
+    {
+        /** @var StatusReporterContract[] $rewindableStatusReporters */
+        $rewindableStatusReporters = \iterable_to_array($statusReporters);
+        $this->statusReporters = $rewindableStatusReporters;
+        $this->logger = $logger;
+    }
+
+    public function next(StatusReportingContextInterface $context): array
+    {
+        $statusReporter = \array_shift($this->statusReporters);
+
+        if (!$statusReporter instanceof StatusReporterContract) {
+            return [];
+        }
+
+        $this->logger->debug('Execute FlowComponent status reporter', [
+            'status_reporter' => $statusReporter,
+        ]);
+
+        return $statusReporter->report($context, $this);
+    }
+}
